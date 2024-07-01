33feb15d203f5558d3ea90eb5b83f8ecb27203c2
cosmocreates <112910885+cosmocreates@users.noreply.github.com>
2024-07-01 05:21:31 -0500

    Delete scripts directory

--- scripts/tools/input.js
+++
@@ -1,8 +0,0 @@
-import { Keyboard, Mouse } from './inputDevices.js'
-
-export class InputHandler {
-    constructor() {
-        this.keyboard = new Keyboard()
-        this.mouse = new Mouse()
-    }
-}
